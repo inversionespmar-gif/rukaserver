b1edcf8b1a4f207d8f4e0dfb31981927c1c4510c
inversionespmar-gif <coopchinokueltda@gmail.com>
2026-07-15 12:19:22 -0300

    fix: import @Composable in MainActivity

--- android/app/src/main/java/com/rukatv/iptv/MainActivity.kt
+++ android/app/src/main/java/com/rukatv/iptv/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
